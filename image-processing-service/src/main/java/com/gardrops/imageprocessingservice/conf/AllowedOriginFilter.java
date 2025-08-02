d888d61012425fe60ae205e6d5d6a86b7c679b0e
Emre Zinal <emrezinal151@gmail.com>
2025-08-02 19:44:23 +0300

    Implement request filter to only allow requests from localhost

---
+++ image-processing-service/src/main/java/com/gardrops/imageprocessingservice/conf/AllowedOriginFilter.java
@@ -0,0 +1,35 @@
+package com.gardrops.imageprocessingservice.conf;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class AllowedOriginFilter extends OncePerRequestFilter {
+    private final List<String> allowedOrigins;
+
+    public AllowedOriginFilter(@Value("${app.allowed-origins}") List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        String remoteAddr = request.getRemoteAddr();
+
+        if (allowedOrigins.contains(remoteAddr)) {
+            chain.doFilter(request, response);
+        } else {
+            ((HttpServletResponse) response).sendError(
+                    HttpServletResponse.SC_FORBIDDEN, "Access denied: only accessible from allowed origins");
+        }
+    }
+}
+
