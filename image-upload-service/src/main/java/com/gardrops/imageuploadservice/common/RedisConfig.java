33163d5bf8fad5fa7f86c1a2dd12d69576e4ec1e
Emre Zinal <emrezinal151@gmail.com>
2025-08-02 20:26:16 +0300

    Edit package names

--- image-upload-service/src/main/java/com/gardrops/imageuploadservice/common/RedisConfig.java
+++ image-upload-service/src/main/java/com/gardrops/imageuploadservice/common/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.gardrops.imageuploadservice.conf;
+package com.gardrops.imageuploadservice.common;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
